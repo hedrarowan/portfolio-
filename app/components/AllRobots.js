b45e0d576da08dbf7676ea6090527feac9b7e07d
Hedra Rowan <hedrarowan@gmail.com>
2020-09-01 11:02:06 -0500

    7 specs passing

--- app/components/AllRobots.js
+++ app/components/AllRobots.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchRobotsThunk } from "../redux/robots";
 
 // Notice that we're exporting the AllRobots component twice. The named export
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
 export class AllRobots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robots: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.props.getRobots();
+  }
+
   render() {
-    return <div />;
+    if (this.props.robots.length === 0 || this.props.robots === undefined) {
+      return <h1>"No Robots"</h1>;
+    } else {
+      return (
+        <ul>
+          {this.props.robots.map((robot) => {
+            return (
+              <li key={robot.id}>
+                <img src={robot.imageUrl} />
+                <span>{robot.name}</span>
+                {/* <span></span> */}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    }
   }
 }
 
-const mapState = () => {
-  return {};
+const mapState = (state) => {
+  return {
+    robots: state.robots,
+  };
 };
 
-const mapDispatch = () => {
-  return {};
+const mapDispatch = (dispatch) => {
+  return {
+    getRobots: () => dispatch(fetchRobots()),
+  };
 };
 
 export default connect(mapState, mapDispatch)(AllRobots);
--- app/redux/robots.js
+++ app/redux/robots.js
@@ -1,9 +1,44 @@
-export const setRobots = () => {};
+import thunkMiddleware from "redux-thunk";
+import axios from "axios";
+export const SET_ROBOTS = "SET_ROBOTS";
+export const setRobots = (robots) => {
+  return {
+    type: SET_ROBOTS,
+    robots: robots,
+  };
+};
 
-export const fetchRobots = () => {};
+export const ROBOT_ERROR = "ROBOT_ERROR";
+
+export const robotError = (error) => {
+  return {
+    type: ROBOT_ERROR,
+    error: error,
+  };
+};
+
+export function fetchRobots() {
+  return async (dispatch) => {
+    try {
+      const res = await axios.get("/api/robots");
+      const robots = await res.data;
+      await dispatch(setRobots(robots));
+    } catch (error) {
+      dispatch(robotError(error));
+    }
+  };
+}
+
+const initialState = [];
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function robotsReducer() {
-  return null;
+export default function robotsReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_ROBOTS:
+      const robots = action.robots;
+      return robots;
+    default:
+      return initialState;
+  }
 }
