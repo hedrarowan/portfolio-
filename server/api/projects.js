2508e29a92ec8b3d07cdbbe511df5c8aec76f96c
Hedra Rowan <hedrarowan@gmail.com>
2020-09-02 09:02:00 -0500

    Feature: Get all robots and projects

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,12 +1,3 @@
 import axios from "axios";
 const router = require("express").Router();
 const { Robot } = require("../db");
-
-// router.get("/projects", async (req, res, next) => {
-//   try {
-//     const projects = await Project.findAll();
-//     res.status(200).send(projects);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
--- server/api/robots.js
+++ server/api/robots.js
@@ -1,12 +1,3 @@
 import axios from "axios";
 const router = require("express").Router();
 const { Robot } = require("../db");
-
-// router.get("/robots", async (req, res, next) => {
-//   try {
-//     const robots = await Robot.findAll();
-//     res.status(200).send(robots);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
