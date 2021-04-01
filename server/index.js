9dc004b98f85854219514d78fb191e5b537c42bf
Hedra Rowan <hedrarowan@gmail.com>
2021-04-01 17:02:37 -0500

    back to square one

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // static middleware
-app.use('/static', express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 
 
